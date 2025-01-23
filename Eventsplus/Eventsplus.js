7b3b7de39006462466fdd7f936f0683630ae674d
Simone3838 <simone.paolini14@gmail.com>
2025-01-23 16:24:35 +0100

    Update Eventsplus.js

--- Eventsplus/Eventsplus.js
+++ Eventsplus/Eventsplus.js
@@ -1,4 +1,3 @@
-// Register the extension
 Scratch.extensions.register('Events Expansion', {
     blocks: [
         {
@@ -8,7 +7,7 @@ Scratch.extensions.register('Events Expansion', {
             arguments: {
                 stopSign: {
                     type: Scratch.ArgumentType.IMAGE,
-                    menu: 'stopSignMenu', // Define a menu for the stop sign images
+                    menu: 'stopSignMenu',
                     defaultValue: 'stopSign'
                 }
             }
@@ -35,29 +34,36 @@ Scratch.extensions.register('Events Expansion', {
     }
 });
 
-// Define the behavior of the "whenStopSignClicked" block
 Scratch.blocks['whenStopSignClicked'] = {
     init: function (obj) {
         obj.appendDummyInput('stopSign').appendField(new Scratch.FieldImage('stopSign', 15, 15, 'Choose stop sign'));
     },
     definition: function (block) {
         var stopSign = block.getFieldValue('stopSign');
-        Scratch.Events.listenTo(stopSign, 'click', function (event) {
-            // Start a thread when the stop sign is clicked
-            this.parentThread.start(true, {
-                stopSign: event.target
+        console.log("Event listener set for stop sign: " + stopSign);
+        
+        // Verify stopSign exists and is accessible before adding event listener
+        if (stopSign) {
+            Scratch.Events.listenTo(stopSign, 'click', function (event) {
+                console.log("Stop sign clicked!");
+                this.parentThread.start(true, {
+                    stopSign: event.target
+                });
             });
-        });
+        } else {
+            console.log("Stop sign not available. Check your input.");
+        }
     }
 };
 
-// Define the behavior of the "when" block (generic condition event)
 Scratch.blocks['when'] = {
     init: function (obj) {
         obj.appendValueInput('condition').setCheck(null).setAlign(Scratch.Blocks.CENTER);
     },
     definition: function (block) {
         var condition = block.getFieldValue('condition');
+        console.log("Condition set: " + condition);
+        
         var callback = function () {
             if (this.evaluate) {
                 this.evaluate(block, this);
@@ -69,21 +75,18 @@ Scratch.blocks['when'] = {
     }
 };
 
-// Define the behavior of the "always" block
 Scratch.blocks['always'] = {
-    init: function (obj) {
-        // No input needed for the "always" block
-    },
+    init: function (obj) {},
     definition: function (block) {
         block.setPreviousStatement(true, null);
         block.setNextStatement(true, null);
+        console.log("Always block initialized.");
         Scratch.always = function (callback, context) {
             callback.call(context);
         };
     }
 };
 
-// Define how events are evaluated
 Scratch.listeners['when'] = {
     evaluate: function (event, callback, context) {
         callback.call(context);
