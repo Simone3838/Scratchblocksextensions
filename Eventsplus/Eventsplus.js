a912f3fb88609be884f707663c8d9cf1f345a2bf
Simone3838 <simone.paolini14@gmail.com>
2025-01-23 16:15:49 +0100

    Update Eventsplus.js

--- Eventsplus/Eventsplus.js
+++ Eventsplus/Eventsplus.js
@@ -1,3 +1,49 @@
+// This is just a sample script. Paste your real code (javascript or HTML) here.
+Scratch.extensions.register('Events Espansion', {
+    blocks: [{
+        opcode: 'whenStopSignClicked',
+        blockType: Scratch.BlockType.EVENT,
+        text: 'when [stop sign v] clicked',
+        arguments: {
+            stopSign: {
+                type: Scratch.ArgumentType.IMAGE,
+                menu: 'Choose stop sign',
+                defaultValue: 'stopSign'
+            }
+        }
+    }, {
+        opcode: 'when',
+        blockType: Scratch.BlockType.EVENT,
+        text: 'when <>',
+        arguments: {
+            condition: {
+                type: Scratch.ArgumentType.TEXT,
+                defaultValue: 'green flag clicked'
+            }
+        }
+    }, {
+        opcode: 'always',
+        blockType: Scratch.BlockType.INFINITE_LOOP,
+        text: 'always'
+    }]
+});
+Scratch.blocks['whenStopSignClicked'] = {
+    init: function(obj) {
+        obj.appendDummyInput("stopSign").appendField(new Scratch.fieldImage("stopSign", 15, 15, "Choose stop sign"));
+    },
+    definition: function(block) {
+        Scratch.Events.listenTo(block, "click", {
+            target: "stopSign",
+            callback: function(event) {
+                this.parentThread.start(true, {
+                    stopSign: event.target
+                });
+            }
+        });
+    }
+};
+Scratch.blocks['when'] = {
+    init: function(obj) {
         obj.appendValueInput("condition").setCheck(null).setAlign(Scratch.Blocks.CENTER);
     },
     definition: function(block) {
